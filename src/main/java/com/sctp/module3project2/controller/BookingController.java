98f8bde27b32ab55b85fa32c6ab687f2905fe772
xxnlxzx <xxnlxzx@gmail.com>
2023-07-25 23:56:03 +0800

    Removed 'userIdService & @RequestHeader

--- src/main/java/com/sctp/module3project2/controller/BookingController.java
+++ src/main/java/com/sctp/module3project2/controller/BookingController.java
@@ -2,8 +2,6 @@ package com.sctp.module3project2.controller;
 
 import java.util.ArrayList;
 
-import javax.naming.AuthenticationException;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,9 +14,7 @@ import com.sctp.module3project2.entity.Booking;
 import com.sctp.module3project2.services.BookingService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.PutMapping;
-// import com.sctp.module3project2.services.UseridService;
 
 // Joel
 
@@ -26,53 +22,38 @@ import org.springframework.web.bind.annotation.PutMapping;
 @RequestMapping("/api/booking")
 public class BookingController {
     private BookingService bookingService;
-    private UseridService useridService;
 
-    public BookingController(BookingService bookingService, UseridService useridService) {
+    public BookingController(BookingService bookingService) {
         this.bookingService = bookingService;
-        this.useridService = useridService;
     }
 
     @GetMapping("")
-    public ResponseEntity<ArrayList<Booking>> getAllBookings(@RequestHeader(value = "password") String password,
-            @RequestHeader(value = "user") String user) throws AuthenticationException {
-        useridService.verifyPassword(user, password);
+    public ResponseEntity<ArrayList<Booking>> getAllBookings() {
         ArrayList<Booking> allBookings = bookingService.getAllBookings();
         return ResponseEntity.ok(allBookings);
 
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Booking> getBooking(@PathVariable Long id, @RequestHeader(value = "password") String password,
-            @RequestHeader(value = "user") String user) throws AuthenticationException {
-        useridService.verifyPassword(user, password);
+    public ResponseEntity<Booking> getBooking(@PathVariable Long id) {
         Booking foundBooking = bookingService.getBooking(id);
         return ResponseEntity.ok(foundBooking);
     }
 
     @PostMapping("")
-    public ResponseEntity<Booking> saveBooking(@RequestBody Booking booking,
-            @RequestHeader(value = "password") String password, @RequestHeader(value = "user") String user)
-            throws AuthenticationException {
-        useridService.verifyPassword(user, password);
+    public ResponseEntity<Booking> saveBooking(@RequestBody Booking booking) {
         Booking newBooking = bookingService.saveBooking(booking);
         return ResponseEntity.ok(newBooking);
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<Booking> updateBooking(@PathVariable Long id, @RequestBody Booking booking,
-            @RequestHeader(value = "password") String password, @RequestHeader(value = "user") String user)
-            throws AuthenticationException {
-        useridService.verifyPassword(user, password);
+    public ResponseEntity<Booking> updateBooking(@PathVariable Long id, @RequestBody Booking booking) {
         Booking updatedBooking = bookingService.updateBooking(id, booking);
         return ResponseEntity.ok(updatedBooking);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Booking> deleteBooking(@PathVariable Long id,
-            @RequestHeader(value = "password") String password, @RequestHeader(value = "user") String user)
-            throws AuthenticationException {
-        useridService.verifyPassword(user, password);
+    public ResponseEntity<Booking> deleteBooking(@PathVariable Long id) {
         bookingService.deleteBooking(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
--- src/main/java/com/sctp/module3project2/controller/ShippingRouteController.java
+++ src/main/java/com/sctp/module3project2/controller/ShippingRouteController.java
@@ -1,13 +1,10 @@
 package com.sctp.module3project2.controller;
 
 import java.util.ArrayList;
-// import java.util.HashMap;
-// import java.util.Map;
 
 import javax.naming.AuthenticationException;
 
 import org.springframework.http.HttpStatus;
-// import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,43 +12,34 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sctp.module3project2.entity.ShippingRoute;
 import com.sctp.module3project2.services.ShippingRouteService;
-// import com.sctp.module3project2.services.UseridService;
 
 // Edited by Wei Kang
 @RestController
 @RequestMapping("/api/shippingRoute")
 public class ShippingRouteController {
     private ShippingRouteService shippingRouteService;
-    private UseridService useridService;
 
-    public ShippingRouteController(ShippingRouteService shippingRouteService, UseridService useridService) {
+    public ShippingRouteController(ShippingRouteService shippingRouteService) {
         this.shippingRouteService = shippingRouteService;
-        this.useridService = useridService;
     }
 
     @PostMapping("")
-    public ResponseEntity<ShippingRoute> saveShippingRoute(@RequestBody ShippingRoute shippingRoute,
-            @RequestHeader(value = "password") String password, @RequestHeader(value = "user") String user)
+    public ResponseEntity<ShippingRoute> saveShippingRoute(@RequestBody ShippingRoute shippingRoute)
             throws AuthenticationException {
-        useridService.verifyPassword(user, password);
         ShippingRoute newShippingRoute = shippingRouteService.saveShippingRoute(shippingRoute);
         return new ResponseEntity<>(newShippingRoute, HttpStatus.CREATED);
 
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ShippingRoute> getShippingRoute(@PathVariable int id,
-            @RequestHeader(value = "password") String password, @RequestHeader(value = "user") String user)
+    public ResponseEntity<ShippingRoute> getShippingRoute(@PathVariable int id)
             throws AuthenticationException {
-        useridService.verifyPassword(user, password);
         ShippingRoute foundShippingRoute = shippingRouteService.getShippingRoute(id);
-
         return new ResponseEntity<>(foundShippingRoute, HttpStatus.OK);
     }
 
@@ -71,11 +59,8 @@ public class ShippingRouteController {
     // }
 
     @GetMapping("")
-    public ResponseEntity<ArrayList<ShippingRoute>> getAllShippingRoute(
-            @RequestHeader(value = "password") String password, @RequestHeader(value = "user") String user)
+    public ResponseEntity<ArrayList<ShippingRoute>> getAllShippingRoute()
             throws AuthenticationException {
-        useridService.verifyPassword(user, password);
-
         ArrayList<ShippingRoute> allShippingRoute = (ArrayList<ShippingRoute>) shippingRouteService
                 .getAllShippingRoutes();
 
@@ -84,20 +69,16 @@ public class ShippingRouteController {
 
     @PutMapping("/{id}")
     public ResponseEntity<ShippingRoute> updateShippingRoute(@PathVariable int id,
-            @RequestHeader(value = "password") String password, @RequestBody ShippingRoute shippingRoute,
-            @RequestHeader(value = "user") String user) throws AuthenticationException {
-        useridService.verifyPassword(user, password);
+            @RequestBody ShippingRoute shippingRoute)
+            throws AuthenticationException {
         ShippingRoute updatedShippingRoute = shippingRouteService.updateShippingRoute(id, shippingRoute);
         return new ResponseEntity<>(updatedShippingRoute, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<HttpStatus> deleteShippingRoute(@PathVariable int id,
-            @RequestHeader(value = "password") String password, @RequestHeader(value = "user") String user)
+    public ResponseEntity<HttpStatus> deleteShippingRoute(@PathVariable int id)
             throws AuthenticationException {
-        useridService.verifyPassword(user, password);
         shippingRouteService.deleteShippingRoute(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
-
 }
